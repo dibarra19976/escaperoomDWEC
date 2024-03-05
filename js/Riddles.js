857de005e98b5c86ff71e2539a00091b7495bf0d
Dani Ibarra <dibarra19976@iesjoanramis.org>
2024-03-05 22:15:40 +0100

    all commented

--- js/Riddles.js
+++ js/Riddles.js
@@ -3,7 +3,9 @@ loadDoc((response) => {
   json = JSON.parse(response);
 }, "/json/riddles.json");
 
+//funcion para empezar el minijuego
 function startRiddle(game) {
+  //comprueba que el juego no haya sido pasado ya
   if (!game.beaten) {
     document.getElementById("userGuess").value = "";
     closeEndedPopup();
@@ -12,20 +14,27 @@ function startRiddle(game) {
     let difficulty = game.difficulty;
     let id = game.id;
 
+    //se carga el json por si no se habia cargado al inicio 
     loadDoc((response) => {
       json = JSON.parse(response);
       loadRiddle();
     }, "/json/riddles.json");
 
+    //funcion para cargar la adivinanza
     function loadRiddle() {
+      //si el elemento de juego no tiene ningun indice de adivinanza
       if (game.index === null) {
+        //obtiene uno aleatorio de la dificultad indicada
         getRandomRiddle(difficulty);
       }
+      //guardamos la adivinanza en una variable
       riddle = json[difficulty][game.index];
       console.log(riddle.answer);
+      //la mostramos por pantalla
       showRiddle(riddle);
     }
 
+    //funcion para mostrar la adivinanza
     function showRiddle(riddle) {
       let riddleText = document
         .getElementById("riddle")
@@ -33,6 +42,7 @@ function startRiddle(game) {
       let userGuess = document.getElementById("userGuess");
       let checkBtn = document.getElementById("riddle").querySelector("button");
 
+      //Añadimos un event listener para comprobar la palabra introducida por el usuario y añadir un sonido al escribir
       userGuess.addEventListener("keydown", (e) => {
         keydown_Sound();
         if (e.key == "Enter") {
@@ -40,27 +50,37 @@ function startRiddle(game) {
         }
       });
 
+      //añadimos otro event listener para lo mismo pero con un boton
       checkBtn.addEventListener("click", () => {
+        select_Sound();
         checkAnswer(riddle, userGuess.value);
       });
 
+      //mostramos por pantalla la adivinanza
       riddleText.innerText = riddle.riddle;
     }
 
+    //funcion para comp¡robar la respuesta
     function checkAnswer(riddle, answer) {
+      //si es la misma
       if (riddle.answer.toLowerCase() === answer.toLowerCase().trim()) {
         correct_Sound();
+        //miramos que no se haya pasado ya (para evitar que la puntuacion se calcule dos veces)
         if(!game.beaten){
           calculateScore(attempts);
         }
+        //lo ponemos como pasado
         setBeaten(id);
       }
+      //si no ha sido adivinado
       else{
+        //aumentan los intentos
         attempts++;
         wrong_Sound();
       }
     }
 
+    //funcion para obtener una adivinanza aleatoria
     function getRandomRiddle(difficulty) {
       game.index = getRandom(0, json[difficulty].length-1);
     }
@@ -72,5 +92,3 @@ function startRiddle(game) {
     openEndedPopup();
   }
 }
-
-// Llamada a la funcion JSON
